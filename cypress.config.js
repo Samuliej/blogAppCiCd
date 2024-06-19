a16199be312e5693722c9b52cfc29dfa8513fd7d
Samuli Toppi <samuli.toppi@outlook.com>
2024-06-19 12:12:30 +0300

    Cypress in actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
+  baseUrl: "http://localhost:3000",
   e2e: {
     setupNodeEvents(on, config) {
 
     },
   },
-});
+})
